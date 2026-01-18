f062e2195857f077a8b696029e31050f591a87c2
Brunniel <brunisdan5@gmail.com>
2026-01-17 20:09:45 -0600

    simple drawing done NO ERASE

--- program.js
+++ program.js
@@ -1,7 +1,18 @@
+/* - - - HTML ELEMENTS - - - */
+
 const grid = document.querySelector("#grid");
 
+
+/* - - - GLOBAL VARIABLES - - - */
+
+let isDrawing = false;
+
+
+/* - - - FUNCTIONS - - - */
+
+// Function to create a N x N grid
 function createNxNGrid(n) {
-    for (let i=0; i<n*n; i++) {
+    for (let i = 0; i < n*n; i++) {
         // Creating the cell
         const cell = document.createElement("div");
         cell.style.boxSizing = "border-box";
@@ -10,7 +21,27 @@ function createNxNGrid(n) {
         cell.style.height = `${size}px`;
         cell.style.width = `${size}px`;
         grid.appendChild(cell);
-    }
+
+
+        // Listeners for the hover effect (for each cell)
+        cell.addEventListener("mousedown", () => {
+            cell.style.background = "black";
+            isDrawing = true;
+        });
+
+        cell.addEventListener("mouseup", () => {
+            isDrawing = false;
+        });
+
+        cell.addEventListener("mouseenter", () => {
+            if (isDrawing) {
+                cell.style.background = "black";
+            }
+        });
+    }let isDrawing = false;
 }
 
-createNxNGrid(10);
+
+/* - - - PROGRAM LOGIC - - - */
+createNxNGrid(16);
+
